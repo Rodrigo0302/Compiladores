12eb5da39bc9d8bdeb80b3e35932a60e14361234
Rodrigo <rodrigo77e@outlook.com>
2024-05-14 13:21:41 -0600

    Se comenzaron a hacer las producciones de la gramatica lli

--- Interprete/src/interprete/Parser.java
+++ Interprete/src/interprete/Parser.java
@@ -13,7 +13,7 @@ public class Parser {
     }
 
     public void parse() throws ParserException{
-        //program();
+        program();
 
         if (preanalisis.getTipo() != TipoToken.EOF) {
             String mensaje = "Se encontro un error en el programa";
@@ -21,6 +21,98 @@ public class Parser {
         }
     }
 
+    private void program() throws Exception{
+        //declaration();
+    }
+
+    private void declaration() throws Exception{
+        switch (preanalisis.getTipo()) {
+            case FUN:
+                funDecl();
+                declaration();
+                break;
+            case VAR:
+                varDecl();
+                declaration();
+                break;
+            case BANG:
+            case MINUS:
+            case TRUE:
+            case FALSE:
+            case NULL:
+            case NUMBER:
+            case STRING:
+            case IDENTIFIER:
+            case LEFT_PAREN:
+            case FOR:
+            case IF:
+            case PRINT:
+            case RETURN:
+            case WHILE:
+            case LEFT_BRACE:
+                statement();
+                declaration();
+                break;
+            default:
+                String mensaje = "Error se esperaba una declaracion pero se encontro " + preanalisis.getTipo();
+                throw new ParserException(mensaje);
+        }
+    }
+
+    private void funDecl() throws Exception{
+        match(TipoToken.FUN);
+        function();
+    }
+
+    private void varDecl() throws Exception{
+        match(TipoToken.VAR);
+        match(TipoToken.IDENTIFIER);
+        varInit();
+        match(TipoToken.SEMICOLON);
+    }
+    private void statement() throws Exception{
+        switch (preanalisis.getTipo()) {
+            case BANG:
+            case MINUS:
+            case TRUE:
+            case FALSE:
+            case NULL:
+            case NUMBER:
+            case STRING:
+            case IDENTIFIER:
+            case LEFT_PAREN:
+                EXPR_STMT();
+                break;
+            case FOR:
+                FOR_STMT();
+                break;
+            case IF:
+                IF_STMT();
+                break;
+            case PRINT:
+                PRINT_STMT();
+                break;
+            case RETURN:
+                RETURN_STMT();
+                break;
+            case WHILE:
+                WHILE_STMT();
+                break;
+            case LEFT_BRACE:
+                BLOCK();
+                break;
+            default:
+                String mensaje = "Error se esperaba una sentencia pero se encontro " + preanalisis.getTipo();
+                throw new ParserException(mensaje);
+        }
+    }
+
+    private void varInit() throws Exception{
+        if (preanalisis.getTipo() == TipoToken.EQUAL) {
+            match(TipoToken.EQUAL);
+            expression();
+        }
+    }
     private void match(TipoToken tt) throws ParserException{
         if (preanalisis.getTipo() == tt) {
             i++;
